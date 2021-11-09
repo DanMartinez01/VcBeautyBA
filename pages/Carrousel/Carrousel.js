4a4c3d06e9396e7ae9ca65a90f7025317b260e35
Daniela Martinez <martinezdl_@hotmail.com>
2021-11-08 21:11:11 -0300

    Create Carrousel component.Add new img

---
+++ pages/Carrousel/Carrousel.js
@@ -0,0 +1,42 @@
+import Image from 'next/dist/client/image';
+import { Carousel } from 'react-bootstrap';
+import CarImg1 from '../Assets/CarImg1.jpg';
+import CarImg5 from '../Assets/CarImg5.jpg';
+import CarImg6 from '../Assets/CarImg5.jpg';
+
+export const Carrousel = () => {
+    return (
+        <Carousel>
+            <Carousel.Item align="center" >
+                <Image
+                    className="d-block w-100 responsive"
+                    src={CarImg1}
+                    alt="First slide"
+
+                />
+                {/* <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption> */}
+            </Carousel.Item>
+            <Carousel.Item align="center" >
+                <Image
+                    className="d-block w-100"
+                    src={CarImg5}
+
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item align="center" >
+                <Image
+                    className="d-block w-100"
+                    src={CarImg6}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
+
+
+    )
+}
+export default Carrousel
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import { Carousel } from 'react-bootstrap';
 import NavComponent from './NavComponent';
+import Carrousel from './Carrousel/Carrousel'
 import InfoSection from './InfoSection/InfoSection';
 import Footer from '../pages/Footer/Footer';
 
 import { SiWhatsapp } from 'react-icons/si';
+
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -25,44 +25,7 @@ function MyApp({ Component, pageProps }) {
         </a>
       </div>
       <NavComponent />
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://media.istockphoto.com/photos/woman-in-mask-on-face-in-spa-beauty-salon-picture-id921797424?k=20&m=921797424&s=612x612&w=0&h=-oKjXqOjxamu9dcOtcF-WggUrXE8mB1DDTmTbCuACrc="
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://thumbs.dreamstime.com/b/face-peeling-mask-spa-beauty-treatment-skincare-woman-getting-facial-care-beautician-spa-salon-close-up-spa-clay-mask-o-face-145250762.jpg"
-
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://www.heavenlyblissbeauty.com.au/uploads/126/facials.jpg"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Carrousel />
       <Component {...pageProps} />
       <InfoSection />
       <Footer />
