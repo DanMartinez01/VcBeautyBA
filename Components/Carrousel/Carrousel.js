09406c5b434f7f1b4117349f75db185f3bc01b84
Daniela Martinez <martinezdl_@hotmail.com>
2022-03-27 19:28:24 -0300

    Carousel changes

--- Components/Carrousel/Carrousel.js
+++ Components/Carrousel/Carrousel.js
@@ -1,7 +1,7 @@
 import Image from 'next/dist/client/image';
 import styles from './Carrousel.module.css';
 import { Carousel } from 'react-bootstrap';
-import CarImg1 from '../../Assets/2.jpg';
+import CarImg1 from '../../Assets/2.png';
 import CarImg4 from '../../Assets/4.jpg';
 import CarImg3 from '../../Assets/3.jpg';
 import React, { Component } from 'react';
@@ -16,23 +16,26 @@ export const Carrousel = () => {
                         className={styles.carouselImg}
                         src={CarImg1}
                         alt="First slide"
-                        height="1100px"
+
+                        height="600px"
                     />
                 </Carousel.Item >
                 <Carousel.Item align="center" className={styles.CarouselItem}>
                     <Image
                         className={styles.carouselImg}
-                        src={CarImg4}
+                        src={CarImg1}
                         alt="Second slide"
-                        height="1100px"
+
+                        height="600px"
                     />
                 </Carousel.Item>
                 <Carousel.Item align="center" className={styles.CarouselItem}>
                     <Image
                         className={styles.carouselImg}
-                        src={CarImg3}
+                        src={CarImg1}
                         alt="Third slide"
-                        height="1100px"
+
+                        height="600px"
                     />
                 </Carousel.Item>
             </Carousel>
